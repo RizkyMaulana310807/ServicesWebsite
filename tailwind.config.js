82ada3b4d2dbe59e481baa7dae9e59ed5f2b43db
RizkyMaulana310807 <rizkymaulana317b@gmail.com>
2024-10-31 19:27:49 +0700

    Initialize

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Add this line to include the root index.html
+    "./.{html, js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
